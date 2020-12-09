a0164dd8aef78667315bdf28d6faf6bf20f04a77
rouan <rouan.laporal@outlook.com>
2020-12-09 12:36:08 +0100

    Update script.js
    ajout div ballon

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const screen = document.getElementsByClassName("screen")[0];
 
+
 screen.addEventListener("click", (e) => {
     const posX = e.clientX;
     const posY = e.clientY;
-})
-
+    const ballon = document.createElement("div");
+    ballon.classList.add("ballon");
+    ballon.id =  'b' + posX + posY;
+  })
