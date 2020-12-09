5af70ad9fd8906dea5eefaf64b781ad3eb2166c6
Nassim ZUBERI <nassim.zuberi@hotmail.fr>
2020-12-09 14:12:11 +0100

    Clic

--- script.js
+++ script.js
@@ -6,15 +6,17 @@ screen.addEventListener("click", (e) => {
     count += 1;
     if(count == 20) {
         const ballon = Array.from(document.getElementsByClassName("ballon"));
-        ballon.map(t => t.classList.add("fall"));
+        ballon.map(t => {
+                t.classList.add("fall");
+                setTimeout(() => screen.removeChild(t), 1000)
+        });
         count = 0;
     }
     const posX = e.clientX;
     const posY = e.clientY;
     const ballon = document.createElement("div");
     ballon.classList.add("ballon");
-    ballon.id =  'b' + posX + posY;
-    ballon.style.top = posY +"px";
-    ballon.style.left = posX + "px"
     screen.appendChild(ballon)
+    ballon.style.top = (posY - ballon.offsetWidth/2) +"px";
+    ballon.style.left =(posX - ballon.offsetWidth/2) + "px"
 })
