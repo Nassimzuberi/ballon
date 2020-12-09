054ca3f4ada8f5ddb1bda29fe98c34dd210c2c10
Nassim ZUBERI <nassim.zuberi@hotmail.fr>
2020-12-09 12:41:08 +0100

    animation

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const screen = document.getElementsByClassName("screen")[0];
+let count = 0;
 
 screen.addEventListener("click", (e) => {
+    count += 1;
+    const ballon = document.getElementsByClassName("ballon")[0];
+    fall(ballon)
+    if(count == 20) {
+        const ballon = Array.from(document.getElementsByClassName("ballon"));
+        ballon.map(t => t.classList.add("fall"));
+        count = 0;
+    }
     const posX = e.clientX;
     const posY = e.clientY;
 })
 
+function fall(div) {
+    console.log( div.style.width )
+    div.style.top = "calc(98% - " + div.style.height + "px)"
+}
